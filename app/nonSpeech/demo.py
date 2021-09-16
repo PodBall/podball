669a2d2351fd1af95b6064fd21d505c51157042e
Julia Seidman <juliaseid@gmail.com>
2021-09-15 17:17:49 -0700

    create jupyter notebook for librosa

--- app/nonSpeech/demo.py
+++ app/nonSpeech/demo.py
@@ -16,25 +16,25 @@ audio_clips = os.listdir(audio_fpath)
 # Load "speech" and "non-speech" files and visualize its waveform (using librosa)
 ns, sr = librosa.load(audio_fpath+audio_clips[0], sr=44100)
 
-# print(ns.shape, sr)
+print(ns.shape, sr)
 
-# plt.figure(figsize=(14, 5))
-# librosa.display.waveplot(ns, sr=sr)
+plt.figure(figsize=(14, 5))
+librosa.display.waveplot(ns, sr=sr)
 
 #Convert the audio waveform to spectrogram
 
-# NS = librosa.stft(ns)
-# NSdb = librosa.amplitude_to_db(abs(NS))
-# plt.figure(figsize=(14, 5))
-# librosa.display.specshow(NSdb, sr=sr, x_axis='time', y_axis='log')
-# plt.colorbar()
+NS = librosa.stft(ns)
+NSdb = librosa.amplitude_to_db(abs(NS))
+plt.figure(figsize=(14, 5))
+librosa.display.specshow(NSdb, sr=sr, x_axis='time', y_axis='log')
+plt.colorbar()
 
-# # Separate harmonics and percussives into two waveforms
-# ns_harmonic, ns_percussive = librosa.effects.hpss(ns)
+# Separate harmonics and percussives into two waveforms
+ns_harmonic, ns_percussive = librosa.effects.hpss(ns)
 
-# #waveform of percussives only
-# plt.figure(figsize=(14, 5))
-# librosa.display.waveplot(ns_percussive, sr=sr)
+#waveform of percussives only
+plt.figure(figsize=(14, 5))
+librosa.display.waveplot(ns_percussive, sr=sr)
 
 # #zero-crossing-rate - x-axis is number of rolling windows of duration frame_length, beginning every hop_length
 ns_zeros = librosa.feature.zero_crossing_rate(ns, frame_length=2048, hop_length=512)
@@ -50,16 +50,16 @@ print(ns_zeros[0].max())
 
 
 #agglomerative segmentation by mel cepstrum
-# ns_mfcc = librosa.feature.mfcc(ns)
-# ns_segments = librosa.segment.agglomerative(ns_mfcc, 30)
-# segment_times = librosa.frames_to_time(ns_segments, sr=sr)
-# print (segment_times)
-# plt.figure(figsize=(14,5))
-# librosa.display.specshow(ns_mfcc, y_axis='mel', x_axis='time')
-# plt.vlines(segment_times, 0, ns_mfcc.shape[1], color='linen', linestyle='--',
-#            linewidth=2, alpha=0.9, label='Segment boundaries')
-# plt.axis('tight')
-# plt.legend(frameon=True, shadow=True)
-# plt.title('Mean Frequency spectrogram')
-# plt.tight_layout()
+ns_mfcc = librosa.feature.mfcc(ns)
+ns_segments = librosa.segment.agglomerative(ns_mfcc, 30)
+segment_times = librosa.frames_to_time(ns_segments, sr=sr)
+print (segment_times)
+plt.figure(figsize=(14,5))
+librosa.display.specshow(ns_mfcc, y_axis='mel', x_axis='time')
+plt.vlines(segment_times, 0, ns_mfcc.shape[1], color='linen', linestyle='--',
+           linewidth=2, alpha=0.9, label='Segment boundaries')
+plt.axis('tight')
+plt.legend(frameon=True, shadow=True)
+plt.title('Mean Frequency spectrogram')
+plt.tight_layout()
 plt.show()
